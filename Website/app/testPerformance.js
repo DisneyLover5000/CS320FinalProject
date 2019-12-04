183be17de30e6950dbdf2c0ee942c08c11f725d4
Katie <katherine.cederdahl@wsu.edu>
2019-12-04 13:35:33 -0800

    Still trying to test performance. Also updated black friday to christmas

--- Website/app/testPerformance.js
+++ Website/app/testPerformance.js
@@ -1,15 +1,11 @@
-describe('Test for Performance on Suggestions', function() {
+describe('Test for Performance', function() {
 
-  describe('Google Form Time', function () {
-    it('The google form should take less than 5 ms and be fully loaded', function () {
-      var document = "suggestions.html";
-      var str = "*forms.gle*";
-      obj = document.NativeWebObject.Find("innerHTML", str);
-      if (obj.Exists)
-        Log.Message("Object containing text '" + str + "' is found", obj.FullName);
-      else
-        Log.Warning("Object containing text '" + str + "' was not found.");
-
-  });
+  describe('Google Form Loading', function () {
+    it('The google form should take less than 5 ms and be loaded', function () {
+      if (document.readyState === "loading"){
+        alert('page is loading');
+        this.timeout(5);
+      }
+    });
   });
 });
