fdbe51ff165db51f943770fb3072adf1346df70f
Katie <katherine.cederdahl@wsu.edu>
2019-12-04 10:48:52 -0800

    Still trying to get a proper testing

--- Website/app/testPerformance.js
+++ Website/app/testPerformance.js
@@ -3,7 +3,7 @@ describe('Test for Performance on Suggestions', function() {
   describe('Google Form Time', function () {
     it('The google form should take less than 5 ms and be fully loaded', function () {
       var document = "suggestions.html";
-      var str = "*forms.gle";
+      var str = "*forms.gle*";
       obj = document.NativeWebObject.Find("innerHTML", str);
       if (obj.Exists)
         Log.Message("Object containing text '" + str + "' is found", obj.FullName);
