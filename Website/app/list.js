963fadf19963941c84af1b7f9b714dba845aa4a3
rebekah-47 <56046870+rebekah-47@users.noreply.github.com>
2019-12-10 17:37:49 -0800

    messed with list

--- Website/app/list.js
+++ Website/app/list.js
@@ -1,11 +1,52 @@
-const clear = document.getElementById("clear");
-const list = document.getElementById("list");
-function addItem() {
-  const currentItem = document.getElementsByName('name');
-  const listItem = document.getElementById.innerHTML("write");
-  listItem.innerHTML = currentItem;
-  list.appendChild(listItem);
+
+class ListItem{
+  constructor(item){
+    this.item = item;
+  }
+
+  show(){
+    console.log(this.item,);
+
+  }
+
 }
-clear.onclick = function() {
-  list.innerHTML = "";
-} 
+
+
+var grabeditem;
+
+class List{
+  constrouctor(ListItem){
+    this.listItem=[];
+
+
+  }
+  grabListItem(Item){
+    'use strict';
+
+    for (let j = 0; j < process.argv.length; j++) {
+      console.log(j + ' -> ' + (process.argv[j]));
+    }
+    // grab item from input process.argv[j]
+
+  }
+  addListItem(Item){
+
+//for (var i=0; i<= this.menuItem.length; i++){
+    Item.grabListItem(Item)=grabeditem;
+    this.listItem.push(grabeditem);
+//}
+  }
+
+
+
+}
+
+const Item = new List();
+Item.addListItem(grabbeditem);
+Item.addListItem(grabbeditem);
+Item.addListItem(grabbeditem);
+Item.addListItem(grabbeditem);
+console.log(Item);
+
+
+
